e4515f8dd70598ee42bf3bf191818ee199326598
Marcel Hugo <37884743+marcelhugo70@users.noreply.github.com>
2022-03-14 20:06:43 -0300

    Após correção da Lista 1 de exercícios.

--- lineares/ListaEstatica.java
+++ lineares/ListaEstatica.java
@@ -3,12 +3,12 @@ package lineares;
 public class ListaEstatica {
 	private int[] info;
 	private int tamanho;
-	
+
 	public ListaEstatica() {
 		info = new int[10];
 		tamanho = 0;
 	}
-	
+
 	public void inserir(int valor) {
 		if (tamanho == info.length) {
 			this.redimensionar();
@@ -18,36 +18,74 @@ public class ListaEstatica {
 	}
 
 	private void redimensionar() {
-		int[] novo = new int[tamanho+10];
-		for (int i=0; i < tamanho; i++) {
+		int[] novo = new int[tamanho + 10];
+		for (int i = 0; i < tamanho; i++) {
 			novo[i] = info[i];
 		}
 		info = novo;
 	}
-	
+
 	public String exibir() {
 		String resultado = "[";
-		for (int i=0; i < tamanho;i++) {
-			resultado += info[i]+", ";
+		for (int i = 0; i < tamanho; i++) {
+			resultado += info[i] + ", ";
 		}
-		return resultado+"]";
+		return resultado + "]";
 	}
-	
+
 	public int buscar(int valor) {
-		for (int i=0; i < tamanho; i++) {
+		for (int i = 0; i < tamanho; i++) {
 			if (info[i] == valor) {
 				return i;
 			}
 		}
 		return -1;
 	}
-	
+
 	public boolean estaVazia() {
 		return (tamanho == 0);
 	}
-	
+
 	public void retirar(int valor) {
-		//...
+		int posicao = this.buscar(valor);
+
+		if (posicao != -1) {
+			for (int idx = posicao; idx < tamanho - 1; idx++) {
+				info[idx] = info[idx + 1];
+			}
+			tamanho--;
+		}
 	}
-	
+
+	public ListaEstatica copiar() {
+		ListaEstatica novaLista = new ListaEstatica();
+
+		for (int idx = 0; idx < tamanho; idx++)
+			novaLista.inserir(info[idx]);
+
+		return novaLista;
+	}
+
+	public void concatenar(ListaEstatica outraLista) {
+		for (int idx = 0; idx < outraLista.getTamanho(); idx++)
+			this.inserir(outraLista.info[idx]); // a pensar
+	}
+
+	public int getTamanho() {
+		return this.tamanho;
+	}
+
+	public ListaEstatica dividir() {
+		ListaEstatica novaLista = new ListaEstatica();
+
+		int tamanhoOriginal = tamanho;
+		int metadeLista = tamanho / 2;
+
+		for (int idx = metadeLista; idx < tamanhoOriginal; idx++)
+			novaLista.inserir(info[idx]);
+
+		this.tamanho = metadeLista;
+
+		return novaLista;
+	}
 }
