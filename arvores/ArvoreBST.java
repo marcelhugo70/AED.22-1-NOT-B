9750e22ae7b1dbb97648486b891f99b04f875542
Marcel Hugo <37884743+marcelhugo70@users.noreply.github.com>
2022-05-09 22:01:26 -0300

    ÁrvoreBST - início da exclusão

--- arvores/ArvoreBST.java
+++ arvores/ArvoreBST.java
@@ -2,19 +2,79 @@ package arvores;
 
 public class ArvoreBST<T extends Comparable<T>> extends ArvoreBinariaAbstract<T> {
 
-	public NoArvoreBST<T> buscar(T valor){
+	public NoArvoreBST<T> buscar(T valor) {
 		if (this.vazia()) {
 			return null;
 		}
-		return ((NoArvoreBST<T>)this.getRaiz()).buscar(valor);
+		return ((NoArvoreBST<T>) this.getRaiz()).buscar(valor);
 	}
-	
+
 	public void inserir(T info) {
-		if (this.vazia()){
+		if (this.vazia()) {
 			this.setRaiz(new NoArvoreBST<>(info));
+		} else {
+			((NoArvoreBST<T>) this.getRaiz()).inserir(info);
 		}
-		else {
-			((NoArvoreBST<T>)this.getRaiz()).inserir(info);
+	}
+
+	public void retirar(T valor) {
+		NoArvoreBST<T> noARetirar = this.buscar(valor);
+		if (noARetirar == null) {
+			return;
 		}
+
+		NoArvoreBST<T> pai = this.descobrirPai(noARetirar);
+		
+		if (noARetirar.ehFolha()) { // caso 1
+			if (pai == null) {  // nó a retirar é raiz
+				
+			}
+			else {
+				
+			}
+		} else {
+			if (noARetirar.temUmFilho()) { // caso 2
+				if (pai == null) {  // nó a retirar é raiz
+					
+				}
+				else {
+					
+				}
+			}
+			else { //caso 3
+				if (pai == null) {  // nó a retirar é raiz
+					
+				}
+				else {
+					
+				}
+			}
+		}
+
+	}
+
+	private NoArvoreBST<T> descobrirPai(NoArvoreBST<T> noARetirar) {
+		if (noARetirar == this.getRaiz()) {
+			return null;
+		}
+		NoArvoreBST<T> noPai = (NoArvoreBST<T>) this.getRaiz();
+
+		while (noPai.getDir() != noARetirar && noPai.getEsq() != noARetirar) {
+			if (noARetirar.getInfo().compareTo(noPai.getInfo()) < 0) {
+				noPai = (NoArvoreBST<T>) noPai.getEsq();
+			} else {
+				noPai = (NoArvoreBST<T>) noPai.getDir();
+			}
+		}
+
+		return noPai;
+	}
+
+
+	public NoArvoreBST<T> buscarPai(T valor) {  // uma alternativa recursiva ao descobrirPai
+		if (this.vazia()) {
+			return null;
+		}
+		return ((NoArvoreBST<T>) this.getRaiz()).buscarPai(valor, null);
 	}
 }
--- arvores/NoArvoreBST.java
+++ arvores/NoArvoreBST.java
@@ -41,4 +41,37 @@ public class NoArvoreBST<T extends Comparable<T>> extends NoArvoreBinaria<T> {
 			}
 		}
 	}
+	
+
+	
+	public NoArvoreBST<T> buscarPai(T valor, NoArvoreBST<T> pai) {
+        if (this.getInfo().equals(valor))
+        {
+            return pai;
+        } else {
+            if (valor.compareTo(this.getInfo()) < 0) {
+                if (this.getEsq() == null) {
+                    return null;
+                } else {
+                    return ((NoArvoreBST<T>) this.getEsq()).buscarPai(valor, this);
+                }
+            } else {
+                if (this.getDir() == null) {
+                    return null;
+                } else {
+                    return ((NoArvoreBST<T>) this.getDir()).buscarPai(valor, this);
+                }
+            }
+        }
+    }
+
+	public boolean ehFolha() {
+		return (this.getEsq() == null && this.getDir() == null);
+	}
+	
+	public boolean temUmFilho() {
+		return (this.getEsq() != null && this.getDir() == null)
+				|| (this.getEsq() == null && this.getDir() != null);
+		//return Boolean.logicalXor(this.getEsq() == null, this.getDir() == null);
+	}
 }
